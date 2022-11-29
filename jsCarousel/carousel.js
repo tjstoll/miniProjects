f3ab71c2dd70a93fd15ba33eb5f45d94b508a8f6
tjstoll <taneishastoll@gmail.com>
2022-11-28 20:43:32 -0800

    renaming variables

--- jsCarousel/carousel.js
+++ jsCarousel/carousel.js
@@ -1,56 +1,58 @@
 "use strict";
 
-var ponies = document.getElementsByClassName("pony");
-var number_of_ponies = ponies.length;
-var index = 0;
+// Collect all slides in the carousel
+var carousel_slides = document.getElementsByClassName("carousel-slide");
+var number_of_slides = carousel_slides.length;
+var index = 0; // for navigating through the slides
 
-var dots_div = document.getElementById("dots");
+// Create navigation dots for each slide in the carousel
+var _navigation_dots = document.getElementById("navigation-dots");
 var dot = document.createElement("i");
 dot.classList.add("fa-solid", "fa-circle", "dot");
 var unique_dot = null;
 
-//add number_of_ponies-1 dots to navigation_dots
-for (let x=1; x<number_of_ponies; x++) {
+//add number_of_slides-1 dots to navigation_dots
+for (let x=1; x<number_of_slides; x++) {
     unique_dot = dot.cloneNode();
     unique_dot.addEventListener("click", function() {jumpTo(x)});
-    dots_div.appendChild(unique_dot);
+    _navigation_dots.appendChild(unique_dot);
 }
 
-var dot_list = dots_div.children;
+var navigation_dots = _navigation_dots.children;
 
-// hide all ponies except the first
-for (let i=1; i<number_of_ponies; i++) {
-    ponies[i].style.display = "none";
+// hide all slides except the first
+for (let i=1; i<number_of_slides; i++) {
+    carousel_slides[i].style.display = "none";
 }
 
 function move(dir) {
-    // get pony at index and set display to hidden
-    ponies[index].style.display = "none";
-    dot_list[index].removeAttribute("id");
+    // get slide at index and set display to hidden
+    carousel_slides[index].style.display = "none";
+    navigation_dots[index].removeAttribute("id");
     
     // increment index by dir
     index += dir;
     
-    // if index is beyond the maximum/minimum index of ponies then set index to 0;
-    if (index > (number_of_ponies-1)) {
+    // if index is beyond the maximum/minimum index of slides then set index to 0;
+    if (index > (number_of_slides-1)) {
         index = 0;
     }
     else if (index < 0) {
-        index = number_of_ponies-1;
+        index = number_of_slides-1;
     }
     else {}
     
-    // find pony at index and set style to display block and left 0
-    ponies[index].style.display = "flex";
-    dot_list[index].id = "active-dot";
+    // find slide at index and set style to display block and left 0
+    carousel_slides[index].style.display = "flex";
+    navigation_dots[index].id = "active-dot";
 }
 
 function jumpTo(ind) {
-    ponies[index].style.display = "none";
-    dot_list[index].removeAttribute("id");
+    carousel_slides[index].style.display = "none";
+    navigation_dots[index].removeAttribute("id");
     
     index = ind;
     
-    ponies[index].style.display = "flex";
-    dot_list[index].id = "active-dot";
+    carousel_slides[index].style.display = "flex";
+    navigation_dots[index].id = "active-dot";
 }
