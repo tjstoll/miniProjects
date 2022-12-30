ca32ed193f0a1ff1da7c41274e840f1f187455c6
tjstoll <taneishastoll@gmail.com>
2022-12-30 10:33:10 -0800

    upgraded functionality

--- guessWordGame/guessWord.js
+++ guessWordGame/guessWord.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const word = 'PLATYPUS';
-let word_progression = ['_', '_', '_', '_', '_', '_', '_', '_'];
+let word;
+const words = [
+        'PLATYPUS',
+        'AARDVARK',
+        'SUNRISE',
+        'CANTELOPE',
+        'BASKETBALL'
+    ];
+
+let word_progression;
 let word_appearance;
 let turns;
 let current_key;
@@ -37,19 +45,44 @@ function drawTurns() {
     document.querySelector("#turns").innerHTML = turn_string;
 }
 
+function gameOver() {
+    if (turns === 0) {
+        document.querySelector("#turns").innerHTML = "YOU LOSE";
+        document.querySelector("#results").style.color = "red";
+        console.log("YOU LOSE");
+        return true;
+    }
+    else if (word_appearance.indexOf('_') < 0) {
+        document.querySelector("#turns").innerHTML = "YOU WIN";
+        document.querySelector("#results").style.color = "green";
+        console.log("YOU WIN");
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 function loopManager() {
     checkGuess();
 
-    if (turns === 0) {
-        document.querySelector("#outcome_lose").style.display = "block";
+    if (gameOver()) {
+        document.querySelectorAll('.kybrdbtn').forEach((btn) => {btn.disabled=true});
+        return 0;
     }
-    else if (word_appearance.indexOf('_') < 0) {
-        console.log("YOU WIN");
-    }
-
+    
     updateWordProgession();
     buildWord();
     drawTurns();
+}
+
+function findWord() {
+    let random_index = Math.floor(Math.random()*words.length);
+    word = words[random_index];
+    
+    for (let i=0; i<word.length; i++) {
+        word_progression.push('_')
+    }
 }
 
 function handle(key) {
@@ -61,6 +94,10 @@ function handle(key) {
 
 function initialize() {
     turns = 5;
+    word_progression = [];
+    document.querySelectorAll('.kybrdbtn').forEach((btn) => {btn.disabled=false});
+    document.querySelector("#results").style.color = "black";
+    findWord();
     buildWord();
     drawTurns();
 }
