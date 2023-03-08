a930583fd58538dd7cab334d4d2d715582ce8292
tjstoll <taneishastoll@gmail.com>
2023-03-07 22:11:47 -0800

    added input handlers

--- showsApp/fetchData.js
+++ showsApp/fetchData.js
@@ -1,36 +1,37 @@
 'use strict';
 
-const baseURL = "https://api.tvmaze.com/";
-let searchURL = baseURL + "search/shows?q=girls";
+// Get the search string
+const input = document.querySelector('#search');
+let searchKey;
+const baseURL = "https://api.tvmaze.com/search/shows?q=";
+let searchURL = baseURL
 let results;
+
+function searchHandler(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        searchKey = input.value.replaceAll(' ', '%');;
+        searchURL += searchKey;
+        console.log(searchURL);
+        input.value = "";
+        // this should execute once the search has been performed
+        searchURL = baseURL;
+    }
+}
+
+input.addEventListener('keypress', searchHandler);
+
 // Delet this line in production
 results = JSON.parse(data);
 
-const test_results = [
-    {
-        id: '123',
-        imgSrc: "https://static.tvmaze.com/uploads/images/medium_portrait/31/78286.jpg",
-        name: 'Girls',
-        rating: 6.8,
-        genres: ['Comedy', 'Drama', 'Romance'],
-        description: "This show is about blah blah blah"
-    },
-    {
-        id: '456',
-        imgSrc: "https://static.tvmaze.com/uploads/images/medium_portrait/406/1015813.jpg",
-        name: 'Another Show',
-        rating: 8.1,
-        genres: ['Dark', 'Drama', 'Thriller'],
-        description: "Definitely not Girls..."
-    }
-]
-
-// fetch(searchURL)
-// .then(response => response.json())
-// .then(data => {
-//     results = data;
-// })
-// .catch(e => {
-//     console.log('Somethine went wrong')
-//     console.log(e)
-// })
+async function getData(url) {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    results = data;
+    })
+    .catch(e => {
+        console.log('Somethine went wrong')
+        console.log(e)
+    })
+}
