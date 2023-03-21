19883a4a896a0907ebcd848bacd2a110ffc19ac7
tjstoll <taneishastoll@gmail.com>
2023-03-21 16:41:59 -0700

    adding canvas functionality

--- tetris/scratchFiles/skeletonTetris.js
+++ tetris/scratchFiles/skeletonTetris.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const consoleGrid = document.querySelector("#console");
 const clear_grid = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
@@ -14,21 +20,67 @@ let grid = structuredClone(clear_grid);
 let grid_width = grid[0].length
 let grid_height = grid.length
 
+const blockSize = 20;
+const blockType = [1,2,3,4,5];
+let currentBlockType = blockType[Math.floor(Math.random()*5)];
+
+const consoleGrid = document.querySelector("#console");
+const ctx = consoleGrid.getContext("2d");
+consoleGrid.height = grid_height*blockSize;
+consoleGrid.width = grid_width*blockSize;
+
+const skeletonGrid = document.querySelector('#skeletonConsole');
+
 let advanceInterval;
 let drawInterval;
 let x=Math.floor(grid_width/2);
 let y=0;
 
 function positionBlock() {
-    new_grid[y][x] = 1;
+    new_grid[y][x] = currentBlockType;
+}
+
+function drawBlock(row, col, colour) {
+    ctx.beginPath();
+    ctx.rect(col*blockSize, row*blockSize,blockSize,blockSize);
+    ctx.fillStyle = colour;
+    ctx.fill();
+    ctx.closePath();
 }
 
 function drawGrid() {
-    let test_string = '';
-    for (let k=0; k<grid_height; k++) {
-        test_string += new_grid[k].join('') + '<br>';
+    ctx.clearRect(0,0,grid_width*blockSize,grid_height*blockSize);
+    
+    for (let row=0; row<grid_height; row++) {
+        for (let col=0; col<grid_width; col++) {
+            
+            switch (new_grid[row][col]) {
+                case 1:
+                    drawBlock(row, col, "cyan");
+                    break;
+                case 2:
+                    drawBlock(row, col, "red");
+                    break;
+                case 3:
+                    drawBlock(row, col, "yellow");
+                    break;
+                case 4:
+                    drawBlock(row, col, "lime");
+                    break;
+                case 5:
+                    drawBlock(row, col, "purple");
+                    break;
+            }
+        }
     }
-    consoleGrid.innerHTML = test_string;
+}
+
+function drawSkeleton() {
+    let test_string = '';
+    for (let row=0; row<grid_height; row++) {
+        test_string += new_grid[row].join('') + '<br>';
+    }
+    skeletonGrid.innerHTML = test_string;
 }
 
 function keyDownHandler(e) {
@@ -36,9 +88,11 @@ function keyDownHandler(e) {
         x -= 1;
     } else if (e.key === "ArrowRight" && x < grid_width-1 && grid[y][x+1] === 0) {
         x += 1;
+    } else if (e.key === "ArrowDown" && y < grid_height-1 && grid[y+1][x] === 0) {
+        y += 1;
     }
     draw();
-    e.preventDefault;
+    e.preventDefault();
 }
 document.addEventListener('keydown', keyDownHandler);
 
@@ -48,6 +102,7 @@ function advance() {
     } else {
         x=Math.floor(grid_width/2);
         y=0;
+        currentBlockType = blockType[Math.floor(Math.random()*5)];
         grid = structuredClone(new_grid);
     }
 }
@@ -56,11 +111,13 @@ function draw() {
     new_grid = structuredClone(grid);
     positionBlock();
     drawGrid();
+    drawSkeleton();
 }
 
 function start() {
     grid = structuredClone(clear_grid);
     y=0;
+
     if (!advanceInterval && !drawInterval) {
         advanceInterval = setInterval(advance, 500)
         drawInterval = setInterval(draw, 500);
