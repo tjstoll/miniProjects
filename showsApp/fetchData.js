86ac4ce2b0e979f8c56e86011175ab3e43d9c265
tjstoll <taneishastoll@gmail.com>
2023-12-12 10:25:03 -0800

    update to match vanilla version

--- showsApp/fetchData.js
+++ showsApp/fetchData.js
@@ -1,37 +1,53 @@
 'use strict';
 
-// Get the search string
-const input = document.querySelector('#search');
-let searchKey;
+let search_data; // = JSON.parse(data);
+let search_results = document.querySelector("#search_results");
+const search_input = document.querySelector('#search_input');
 const baseURL = "https://api.tvmaze.com/search/shows?q=";
-let searchURL = baseURL
-let results;
-
-// function searchHandler(e) {
-//     if (e.key === 'Enter') {
-//         e.preventDefault();
-//         searchKey = input.value.replaceAll(' ', '%');;
-//         searchURL += searchKey;
-//         console.log(searchURL);
-//         input.value = "";
-//         // this should execute once the search has been performed
-//         searchURL = baseURL;
-//     }
-// }
-
-// input.addEventListener('keypress', searchHandler);
-
-// Delet this line in production
-results = JSON.parse(data);
 
 async function getData(url) {
-    fetch(url)
+    await fetch(url)
     .then(response => response.json())
     .then(data => {
-    results = data;
+    search_data = data;
     })
     .catch(e => {
-        console.log('Somethine went wrong')
-        console.log(e)
-    })
+        console.log('Somethine went wrong');
+        console.log(e);
+    });
+}
+
+async function handleSearchInput(e) {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        let search_value = search_input.value.replaceAll(' ', '+');
+        let searchURL = baseURL + search_value;
+        
+        try {
+            await getData(searchURL);
+            SearchResults(search_data);
+            search_input.value = '';
+        } catch(e) {
+            console.log(e);
+        }
+    }
+}
+
+search_input.addEventListener('keypress', handleSearchInput);
+
+function SearchResults(data) {
+    search_results.innerHTML = "";
+    console.log(data);
+    // search_results.appendChild(Message(search_input.value));
+    // if (data.length > 0) {
+    //     const results = data.map((d) => {
+    //         return Result(d.show);
+    //     });
+    
+    //     results.forEach((res) => {
+    //         search_results.appendChild(res);
+    //     });   
+    // } else {
+    //     search_results.appendChild(NoResults());
+    // }
 }
