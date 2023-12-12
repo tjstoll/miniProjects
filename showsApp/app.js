001a165d3994cdcd5e2e1d3e45911cc1eb070ab3
tjstoll <taneishastoll@gmail.com>
2023-12-12 12:07:48 -0800

    initial react implementation

--- showsApp/app.js
+++ showsApp/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
-let search_data; // = JSON.parse(data);
+let search_data = JSON.parse(data);
 let search_results = document.querySelector("#search_results");
 const search_input = document.querySelector('#search_input');
 const baseURL = "https://api.tvmaze.com/search/shows?q=";
 
+
+// Handle Input ---------------------------------------------------------------
 async function getData(url) {
     await fetch(url)
     .then(response => response.json())
@@ -33,24 +35,67 @@ async function handleSearchInput(e) {
     }
 }
 
-search_input.addEventListener('keypress', handleSearchInput);
+// search_input.addEventListener('keypress', handleSearchInput);
 
-function SearchResults(data) {
-    search_results.innerHTML = "";
-    console.log(data);
-    // search_results.appendChild(Message(search_input.value));
-    // if (data.length > 0) {
-    //     const results = data.map((d) => {
-    //         return Result(d.show);
-    //     });
-    
-    //     results.forEach((res) => {
-    //         search_results.appendChild(res);
-    //     });   
-    // } else {
-    //     search_results.appendChild(NoResults());
-    // }
+// React ----------------------------------------------------------------------
+function Title({title, rating, genreList}) {
+    const ratings = rating ? rating : "No Rating";
+    const genres = genreList.length > 0 ? `Genres: ${genreList.join(', ')}` : "No genres listed";
+    return (
+        <div className="title">
+            <h3>{title}</h3>
+            <p>{ratings}</p>
+            <p>{genres}</p>
+        </div>
+    );
+}
+
+function Description({summary}) {
+    function clean_text(text) {
+        return text.replaceAll(/<p>|<\/p>|<b>|<\/b>|<i>|<\/i>/g, '');
+    }
+    const formatted_summary = summary ? clean_text(summary) : "No Description Available";
+    return (
+        <div className="description">
+            <p>{formatted_summary}</p>
+        </div>
+    );
+}
+
+function Poster({imgSrc}) {
+    try {
+        return (
+            <div className="poster">
+                <img src={imgSrc.medium} />;
+            </div>
+        )
+    } catch (e) {
+        return (
+            <div className="Poster">
+                <p>NO IMAGE<br/>AVAILABLE</p>
+            </div>
+            )
+    }
+}
+
+function Result({showData}) {
+    return (
+        <section className="result">
+            <Poster imgSrc={showData.image} />
+            <div className="info">
+                <Title title={showData.name} rating={showData.rating.average} genreList={showData.genres}/>
+                <Description summary={showData.summary}/>
+            </div>
+        </section>
+    );
+}
+
+
+// Build ----------------------------------------------------------------------
+function SearchResults({data}) {
+    const results = data.map((d) => <Result key={d.show.id} showData={d.show} />);
+    return <>{results}</>;
 }
 
 const root = ReactDOM.createRoot(search_results);
-root.render(<h1>Hello, World!</h1>);
+root.render(<SearchResults data={search_data} />);
